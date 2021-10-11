dd3e93efa1895daba3be4d619559f3deb01da2d0
Adhali <adhalio@lpth0234.mskcc.root.mskcc.org>
2021-10-11 10:51:56 -0400

    Updating README File

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-*************************  Interview Problems **************************
+\***\*\*\*\*\*\*\***\*\***\*\*\*\*\*\*\*** Interview Problems \***\*\*\*\*\*\*\***\*\*\***\*\*\*\*\*\*\***
+
+1. lists
+2. Tree
+3. Stacks, Queues.
+4. Binary Search
+5. Dynamic Programming
--- graph/adjacency_list_implementation.py
+++ graph/adjacency_list_implementation.py
@@ -16,16 +16,17 @@ Graph:
 { key: { vertex: { nbr:weight }}}
 """
 
+
 class Vertex:
-    def __init__(self,key):
+    def __init__(self, key):
         self.id = key
         self.connectedTo = {}
 
-    def addNeighbor(self,nbr,weight=0):
+    def addNeighbor(self, nbr, weight=0):
         self.connectedTo[nbr] = weight
 
     def __str__(self):
-        return str(self.id) + ' connectedTo: ' + str([(x,y)  for x, y in self.connectedTo.items()])
+        return str(self.id) + ' connectedTo: ' + str([(x, y) for x, y in self.connectedTo.items()])
 
     def getConnections(self):
         return self.connectedTo.keys()
@@ -33,31 +34,31 @@ class Vertex:
     def getId(self):
         return self.id
 
-    def getWeight(self,nbr):
+    def getWeight(self, nbr):
         return self.connectedTo[nbr]
-    
+
 
 class Graph:
     def __init__(self):
         self.vertList = {}
         self.numVertices = 0
 
-    def addVertex(self,key):
+    def addVertex(self, key):
         self.numVertices = self.numVertices + 1
         newVertex = Vertex(key)
         self.vertList[key] = newVertex
         return newVertex
 
-    def getVertex(self,n):
+    def getVertex(self, n):
         if n in self.vertList:
             return self.vertList[n]
         else:
             return None
 
-    def __contains__(self,n):
+    def __contains__(self, n):
         return n in self.vertList
 
-    def addEdge(self,f,t,cost=0):
+    def addEdge(self, f, t, cost=0):
         if f not in self.vertList:
             nv = self.addVertex(f)
         if t not in self.vertList:
@@ -69,10 +70,16 @@ class Graph:
 
     def __iter__(self):
         return iter(self.vertList.values())
-    
-    
+
+
 if __name__ == "__main__":
     v1 = Vertex(1)
-    v1.addNeighbor(2,1)
+    v1.addNeighbor(2, 1)
     print(v1)
     print(v1.getConnections())
+
+
+'M21-30418', 'M21-30515', 'M21-30502', 'M21-30430', 'M21-30424', 'M21-30435', 'M21-30494', 'M21-30497', 'M21-30507', 'M21-30513', 'M21-30514', 'M21-30537', 'M21-30538', 'M21-30542', 'M21-30543', 'M21-30544', 'M21-30546', 'M21-30628', 'M21-30630', 'M21-30631', 'M21-30678', 'M21-30693', 'M21-30713', 'M21-30714', 'M21-30715', 'M21-30716', 'M21-30717', 'M21-30718', 'M21-30719', 'M21-30738', 'M21-30739', 'M21-30744', 'M21-30745', 'M21-30758', 'M21-30759', 'M21-30791', 'M21-30792', 'M21-30793', 'M21-30811', 'M21-30880', 'M21-30988',
+
+
+"M21-30424", "M21-30430", "M21-30435", "M21-30418", "M21-30494", "M21-30497", "M21-30502", "M21-30513", "M21-30514", "M21-30507", "M21-30515", "M21-30537", "M21-30538", "M21-30628", "M21-30630", "M21-30631", "M21-30678", "M21-30693", "M21-30713", "M21-30715", "M21-30718", "M21-30714", "M21-30716", "M21-30717", "M21-30719", "M21-30739", "M21-30738", "M21-30744", "M21-30745", "M21-30758", "M21-30791", "M21-30793", "M21-30792", "M21-30880", "M21-30988",
